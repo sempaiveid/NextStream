381a7fe5c33c758e6a95c24fbf27185dc4af6cb4
sempaiveid <vladuk640@gmail.com>
2026-05-09 21:08:07 +0300

    add: linting for imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,112 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import boundaries from "eslint-plugin-boundaries";
+import importPlugin from "eslint-plugin-import";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  {
+    plugins: {
+      import: importPlugin,
+      boundaries,
+    },
+
+    settings: {
+      "import/resolver": {
+        typescript: true,
+      },
+
+      "boundaries/elements": [
+        {
+          type: "shared",
+          pattern: "src/shared/*",
+        },
+        {
+          type: "entities",
+          pattern: "src/entities/*",
+        },
+        {
+          type: "features",
+          pattern: "src/features/*",
+        },
+        {
+          type: "widgets",
+          pattern: "src/widgets/*",
+        },
+        {
+          type: "pages",
+          pattern: "src/pages/*",
+        },
+        {
+          type: "app",
+          pattern: "src/app/*",
+        },
+      ],
+    },
+
+    rules: {
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+
+      "import/no-cycle": "error",
+
+      "boundaries/element-types": [
+        "error",
+        {
+          default: "disallow",
+          rules: [
+            {
+              from: "shared",
+              allow: ["shared"],
+            },
+
+            {
+              from: "entities",
+              allow: ["shared", "entities"],
+            },
+
+            {
+              from: "features",
+              allow: ["shared", "entities", "features"],
+            },
+
+            {
+              from: "widgets",
+              allow: ["shared", "entities", "features", "widgets"],
+            },
+
+            {
+              from: "pages",
+              allow: [
+                "shared",
+                "entities",
+                "features",
+                "widgets",
+                "pages",
+              ],
+            },
+
+            {
+              from: "app",
+              allow: ["*"],
+            },
+          ],
+        },
+      ],
+    },
+  },
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
